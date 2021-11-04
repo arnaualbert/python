f95865a07222bb21b6c0591fad303b147399f7ab
Arnau Albert <arnaualbert2003@gmail.com>
2021-11-04 18:08:56 +0100

    v1

---
+++ cuadrado.py
@@ -0,0 +1,7 @@
+from turtle import *
+setup(500,500,0,0)
+def get_upper_left_X(x:float,
+                    y:float,
+                    lenght:float):
+    
+    pass
--- pruebas.py
+++ pruebas.py
@@ -2,5 +2,4 @@
 
 def say_hello():
     print("hello Arnau")
-def 
 say_hello()
