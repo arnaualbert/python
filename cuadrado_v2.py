2be2fdef32f57b0380db99cadc066465f4a33bf9
Arnau Albert <arnaualbert2003@gmail.com>
2021-11-17 19:38:38 +0100

    v1

---
+++ cuadrado_v2.py
@@ -0,0 +1,78 @@
+class Square:
+    x:     float
+    y:     float
+    lenght:float
+
+    def __init__(self, x: float, y: float, lenght: float):
+        "Constructor"
+
+        self.x = x
+        self.y = y
+        self.lenght = lenght
+
+    def get_upper_left_x(self) ->float :
+        "la x del vertice superior izquierdo es "
+        
+        midlenght: float = self.lenght/2
+
+        upper_left_x = self.x - midlenght
+        upper_left_y = self.y + midlenght
+        result = upper_left_x
+        return result
+
+    def get_upper_left_y(self) ->float :
+        "la x del vertice superior izquierdo es "
+        
+        midlenght: float = self.lenght/2
+
+        upper_left_x = self.x - midlenght
+        upper_left_y = self.y + midlenght
+        result = upper_left_y
+        return result
+
+    def get_upper_right_x(self) ->float :
+        "la x del vertice superior izquierdo es "
+        
+        midlenght: float = self.lenght/2
+
+        upper_right_x = self.x - midlenght
+        upper_right_y = self.y + midlenght
+        result = upper_right_x
+        return result
+
+    def get_upper_right_y(self) ->float :
+        "la x del vertice superior izquierdo es "
+        
+        midlenght: float = self.lenght/2
+
+        upper_right_x = self.x - midlenght
+        upper_right_y = self.y + midlenght
+        result = upper_right_y
+        return result
+
+s: Square = Square(0,0,10)
+
+print("Atributos de el quadrado s: ")
+print(s.x)
+print(s.y)
+print(s.lenght)
+
+print("vertice superior izquierdo de el cuadrado s: ")
+print(s.get_upper_left_x())
+print(s.get_upper_left_y())
+
+print("vertice superior derecho de el cuadrado s: ")
+print(s.get_upper_right_x())
+print(s.get_upper_right_y())
+
+
+
+
+
+
+
+
+
+
+
+
--- pruebas.py
+++ pruebas.py
@@ -1,5 +1,21 @@
-#hola
+s1: str = "esto es un string. UN string es un texto"
+s2: str = 'esto es un string. UN string es un texto'
 
-def say_hello():
-    print("hello Arnau")
-say_hello()
+s3: str = "it's a trap!"
+s4: str = 'she said "i love it"'
+s5: str = """"she said "i'm bussy!"""""
+
+s6: str = "las comillas simples y las dobles son identicas"
+
+s7: str = "a" #string de una sola letra no hay char en python
+
+s8: str = """comillas triples"""
+
+#---------------------------------------------------------------------------------------------------
+
+s1: str = "hello"
+s1[1] = "o"
+s1
+dir(s1)
+
+s1.upper()
