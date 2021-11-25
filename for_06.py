2889ba6ad974938c4f5c18b1a578b663fe71fc0c
Arnau Albert <arnaualbert2003@gmail.com>
2021-11-25 17:05:09 +0100

    v10

---
+++ for_06.py
@@ -0,0 +1,11 @@
+# crear una lista con nombres y otra vacia
+# copiar todos los nombres en la lista vacia.
+#--------------------------------------------------------------------------------------------------------------
+
+names: list[str] = ["John", "Mary", "Lucy"]
+new_list: list[str] = []
+
+for name in names:
+    new_list.append(name)
+
+print(new_list)
