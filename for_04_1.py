4cee28afb1ffed5c79a0d6c0f1d226dd5cd81a7d
Arnau Albert <arnaualbert2003@gmail.com>
2021-11-25 16:40:58 +0100

    v10

--- for_04_1.py
+++ for_04_1.py
@@ -1,13 +1,6 @@
 nums: list[int] = [1, 2, 3]
 sumnums = 0
 
-# num1: int = int(input("Escribe el primer numero : "))
-# nums.append(num1)
-# num2: int = int(input("Escribe el segundo numero : "))
-# nums.append(num2)
-# num3: int = int(input("Escribe el tercer numero : "))
-# nums.append(num3)
-
 for number in nums:
     sumnums += number
 
