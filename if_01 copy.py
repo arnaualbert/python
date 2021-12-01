476b01d5181b8372f336178596746f3af444f513
Arnau Albert <arnaualbert2003@gmail.com>
2021-12-01 19:46:20 +0100

    v10

---
+++ if_01 copy.py
@@ -0,0 +1,20 @@
+import pprint
+import random
+import copy
+
+edad: int = 91 # años
+
+if (0 <= edad <= 1): # si se cumplen las dos cosas
+    print("regalar pañales")
+else:
+    # edad > 1 
+    if (1 <= edad <= 6):
+        print("regalar pelota")
+    else:
+         # edad > 6 
+        if (6 <= edad <= 90):
+            print("regalar ropa")
+        else:
+
+            if(90 < edad):
+                print("regalar pañales")
