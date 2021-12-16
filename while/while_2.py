3be640a531ff332c98b670728d4d37d3fc0d2f81
Arnau Albert <arnaualbert2003@gmail.com>
2021-12-16 17:33:18 +0100

    while

---
+++ while/while_2.py
@@ -0,0 +1,18 @@
+# whiles loops
+#while ejecuta el body mientras la condicion sea true
+
+
+
+count: int = 0
+finished: bool = (count > 3)
+
+
+while not finished:
+
+    #body
+    print(count)
+
+    #update finishing condition
+    count = count + 1
+    finished = (count > 3)
+
