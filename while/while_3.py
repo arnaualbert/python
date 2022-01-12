cfef68ebe3102c2acab0610055093c1f541637da
Arnau Albert <arnaualbert2003@gmail.com>
2022-01-12 19:02:17 +0100

    act

--- while/while_3.py
+++ while/while_3.py
@@ -1,11 +1,11 @@
 # una funcion que recive una lista y que devuelva un bool  true si encuentra el numero 3
 
 def find_num3(num_list: list[int]) -> bool:
-    
+
     index: int = 0
     found: bool = False
-    out_of_range: bool = (index==len(num_list))
-    finished: bool = found or out_of_range 
+    out_of_range: bool = (index == len(num_list))
+    finished: bool = found or out_of_range
 
     while not finished:
 
@@ -15,5 +15,5 @@ def find_num3(num_list: list[int]) -> bool:
         index = index + 1
         out_of_range = (index == len(num_list))
         finished = found or out_of_range
-    
+
     return found
