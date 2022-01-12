c138abd54a60cf9740eed5ef707f20762cabeef1
Arnau Albert <arnaualbert2003@gmail.com>
2022-01-12 19:01:24 +0100

    act

--- while/while_1.py
+++ while/while_1.py
@@ -1,6 +1,5 @@
 # whiles loops
-#while ejecuta el body mientras la condicion sea true
-
+# while ejecuta el body mientras la condicion sea true
 
 
 count: int = 0
@@ -9,10 +8,9 @@ finished: bool = (count > 3)
 
 while not finished:
 
-    #body
+    # body
     print(count)
 
-    #update finishing condition
+    # update finishing condition
     count = count + 1
     finished = (count > 3)
-
