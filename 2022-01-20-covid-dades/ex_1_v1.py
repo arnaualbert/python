f0359ba6cfb389dabc2604058207146e06f00095
Arnau Albert <arnaualbert2003@gmail.com>
2022-01-26 21:24:25 +0100

    act

--- 2022-01-20-covid-dades/ex_1_v1.py
+++ 2022-01-20-covid-dades/ex_1_v1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 def get_file(csv_file: str) -> str:
-    contents: str = Path("2022-01-20-covid-dades-simple.csv").read_text()
+    contents: str = Path("/home/arnaualbert/Desktop/Python/2022-01-20-covid-dades/2022-01-20-covid-dades-simple.csv").read_text()
     return contents
 
 def get_rows(contents:str) -> list[str]:
@@ -11,7 +11,8 @@ def get_rows(contents:str) -> list[str]:
     return rows
 
 def get_column(rows: list[str]) -> list[list[str]]:
-    table: list[list[str]] = ";".join(rows)
+    table: list[list[str]] = [[]]
+    str(rows)
     return table
 
 def get_dosis(table: list[list[str]]) -> list[list[int]]:
@@ -20,4 +21,6 @@ def get_dosis(table: list[list[str]]) -> list[list[int]]:
 def get_sum(dosis: list[list[int]]) -> tuple[int,int,int]:
     pass
 
-get_file("2022-01-20-covid-dades-simple.csv")
+get_file("/home/arnaualbert/Desktop/Python/2022-01-20-covid-dades/2022-01-20-covid-dades-simple.csv")
+print(get_rows(get_file("/home/arnaualbert/Desktop/Python/2022-01-20-covid-dades/2022-01-20-covid-dades-simple.csv")))
+print(get_column(get_rows(get_file("/home/arnaualbert/Desktop/Python/2022-01-20-covid-dades/2022-01-20-covid-dades-simple.csv"))))
