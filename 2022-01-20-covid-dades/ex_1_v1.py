e0b06b7dbcb61a918e189465e043247f40ce40db
Arnau Albert <arnaualbert2003@gmail.com>
2022-01-26 20:56:18 +0100

    act

--- 2022-01-20-covid-dades/ex_1_v1.py
+++ 2022-01-20-covid-dades/ex_1_v1.py
@@ -5,14 +5,19 @@ def get_file(csv_file: str) -> str:
     return contents
 
 def get_rows(contents:str) -> list[str]:
-    # lines: str = contents.split("\n")
-    pass
+    lines: str = contents.split("\n")
+    rows: list[str] = []
+    rows.append(lines)
+    return rows
 
 def get_column(rows: list[str]) -> list[list[str]]:
-    pass
+    table: list[list[str]] = ";".join(rows)
+    return table
 
 def get_dosis(table: list[list[str]]) -> list[list[int]]:
     pass
 
 def get_sum(dosis: list[list[int]]) -> tuple[int,int,int]:
     pass
+
+get_file("2022-01-20-covid-dades-simple.csv")
