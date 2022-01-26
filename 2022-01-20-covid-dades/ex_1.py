949155b9ffea1685f5f4f3f69f144f2b8284b196
Arnau Albert <arnaualbert2003@gmail.com>
2022-01-26 19:56:05 +0100

    act

---
+++ 2022-01-20-covid-dades/ex_1.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+def get_file(csv_file: str) -> str:
+    corona_file: str = Path("2022-01-20-covid-dades-aga.csv").read_text()
+    pass
+
+def get_lines(contents:str) -> list[str]:
+    pass
+
+def get_column(lines: list[str]) -> list[int]:
+    pass
+
+def get_dosis(column: list[int]) -> int:
+    pass
+
+def get_sum(dosis: int) -> int:
+    pass
