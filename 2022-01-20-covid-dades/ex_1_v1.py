efdb8b31f08400df382460323750e907e838f70c
Arnau Albert <arnaualbert2003@gmail.com>
2022-01-27 16:16:34 +0100

    act

--- 2022-01-20-covid-dades/ex_1_v1.py
+++ 2022-01-20-covid-dades/ex_1_v1.py
@@ -1,18 +1,21 @@
 from pathlib import Path
 
 def get_file(csv_file: str) -> str:
-    contents: str = Path("/home/arnaualbert/Desktop/Python/2022-01-20-covid-dades/2022-01-20-covid-dades-simple.csv").read_text()
-    return contents
+    contents: str = Path("2022-01-20-covid-dades-simple.csv").read_text()
+    stripped_contents: str = contents.strip()
+    return stripped_contents
 
 def get_rows(contents:str) -> list[str]:
-    lines: str = contents.split("\n")
-    rows: list[str] = []
-    rows.append(lines)
-    return rows
+    lines: list[str] = contents.split("\n")
+    return lines
 
-def get_column(rows: list[str]) -> list[list[str]]:
-    table: list[list[str]] = [[]]
-    str(rows)
+def get_columns(rows: list[str]) -> list[list[str]]:
+    table: list[list[str]] = []
+
+    for row in rows:
+        splitted_row: list[str] = row.split(";")
+        table.append(splitted_row)
+
     return table
 
 def get_dosis(table: list[list[str]]) -> list[list[int]]:
@@ -21,6 +24,17 @@ def get_dosis(table: list[list[str]]) -> list[list[int]]:
 def get_sum(dosis: list[list[int]]) -> tuple[int,int,int]:
     pass
 
-get_file("/home/arnaualbert/Desktop/Python/2022-01-20-covid-dades/2022-01-20-covid-dades-simple.csv")
-print(get_rows(get_file("/home/arnaualbert/Desktop/Python/2022-01-20-covid-dades/2022-01-20-covid-dades-simple.csv")))
-print(get_column(get_rows(get_file("/home/arnaualbert/Desktop/Python/2022-01-20-covid-dades/2022-01-20-covid-dades-simple.csv"))))
+# get_file("/home/arnaualbert/Desktop/Python/2022-01-20-covid-dades/2022-01-20-covid-dades-simple.csv")
+# print(get_rows(get_file("/home/arnaualbert/Desktop/Python/2022-01-20-covid-dades/2022-01-20-covid-dades-simple.csv")))
+# print(get_column(get_rows(get_file("/home/arnaualbert/Desktop/Python/2022-01-20-covid-dades/2022-01-20-covid-dades-simple.csv"))))
+
+
+contents: str             = get_file("2022-01-20-covid-dades-simple.csv")
+rows:     list[str]       = get_rows(contents)
+table:    list[list[str]] = get_columns(rows)
+
+for row in table:
+    print(row)
+
+
+
