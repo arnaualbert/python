63cd7f9e7679c362ff22fb328839ea4bb51fe036
Arnau Albert <arnaualbert2003@gmail.com>
2022-02-24 15:30:22 +0100

    act

--- practicaexamen/ej3/ej3.py
+++ practicaexamen/ej3/ej3.py
@@ -1,23 +1,25 @@
 # import psutil
 import os
 from pathlib import Path
-def get_espacio(saber_espacio: Path):
+# def get_espacio(saber_espacio: Path):
 
 #    total = os.stat(saber_espacio).st_size
 #     sub_dirs: list[Path] = Path(saber_espacio).rglob('*')
     # sub_dirs: list[Path] = Path('/home').glob('*')
-    total: Path = Path(saber_espacio).stat().st_size
-    
-    return total
+    # total: Path = Path(saber_espacio).stat().st_size
+    # sub_dirs: list[Path] = Path(saber_espacio).glob('*')
+    # for dir in sub_dirs:
+    #     x: int = dir.stat().st_size
+    # total: Path = Path(x).stat().st_size
 
-print(get_espacio('/home'))
+
+# print(get_espacio('/home'))
 
 # sub_dirs: list[Path] = Path('/home').glob('*')
 # dir: Path = Path('/home').stat().st_size
 # print(dir)
 
-# sub_dirs: list[Path] = Path('/home').rglob('*')
-# for dir in sub_dirs:
-#     x: int = dir.stat().st_size
-
-#     print(x)
+sub_dirs: list[Path] = Path('/home').glob('*')
+for dir in sub_dirs:
+    x: int = dir.stat().st_size
+print(x)
