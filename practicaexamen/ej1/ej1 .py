edf0b86980e12e74921f27d20fd9419b48504a5c
Arnau Albert <arnaualbert2003@gmail.com>
2022-02-24 17:35:08 +0100

    act

--- practicaexamen/ej1/ej1 .py
+++ practicaexamen/ej1/ej1 .py
@@ -6,23 +6,8 @@ import engine
 
 def get_txt(txt_file: Path, txt_template: Path):
 
-    #coge el archivo lo lee y le saca los espacios
     inv: str = Path("friends.txt").read_text()
     names_list: list[str] = inv.split(",")
-    # strp_txt: str = inv.strip()
-    #poner los cuatro nombres en la lista
-    # lista_nombres: list[str] = strp_txt.split(",")
-    # lista_str_nombres: list[str] = [(txt_str) for txt_str in lista_nombres]
-    # metadata_list: list[dict] = [(txt_str) for txt_str in lista_nombres]
-    # print(metadata_list)
-    #llenar espacios
-    # llenar_espacios : str = "invitation.txt"
-    # vars_dict : dict = {"entry_list": zip(lista_str_nombres,meta)}
-    # txt_str: str = engine.fill_template(llenar_espacios,vars_dict)
-    # list_nombres: list[str] = list(inv)
-    # name_list = [inv]
-    # for nombre in inv:
-    #     name_list.append(nombre)
     for name in names_list:
         tmp_dir: Path = Path(".")
         var_dict: dict = {"name": name}
