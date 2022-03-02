c9c410ff23ecd64e1a572dedc1a2d656e98871d8
Arnau Albert <arnaualbert2003@gmail.com>
2022-03-02 09:45:46 +0100

    act

--- practicaexamen/ej3/ej3.py
+++ practicaexamen/ej3/ej3.py
@@ -1,25 +1,26 @@
 # import psutil
-import os
+# import os
 from pathlib import Path
-# def get_espacio(saber_espacio: Path):
+
+def get_espacio(saber_espacio: Path):
 
 #    total = os.stat(saber_espacio).st_size
-#     sub_dirs: list[Path] = Path(saber_espacio).rglob('*')
-    # sub_dirs: list[Path] = Path('/home').glob('*')
-    # total: Path = Path(saber_espacio).stat().st_size
-    # sub_dirs: list[Path] = Path(saber_espacio).glob('*')
-    # for dir in sub_dirs:
-    #     x: int = dir.stat().st_size
-    # total: Path = Path(x).stat().st_size
+#    sub_dirs: list[Path] = Path(saber_espacio).rglob('*')
+#    sub_dirs: list[Path] = Path('/home').glob('*')
+#    total: Path = Path(saber_espacio).stat().st_size
+   sub_dirs: list[Path] = Path(saber_espacio).glob('*')
+   for dir in sub_dirs:
+        x: int = dir.stat().st_size
+        dir: Path = Path(x).stat().st_size
 
 
-# print(get_espacio('/home'))
+print(get_espacio('.'))
 
 # sub_dirs: list[Path] = Path('/home').glob('*')
 # dir: Path = Path('/home').stat().st_size
 # print(dir)
 
-sub_dirs: list[Path] = Path('/home').glob('*')
-for dir in sub_dirs:
-    x: int = dir.stat().st_size
-print(x)
+# sub_dirs: list[Path] = Path('/home').glob('*')
+# for dir in sub_dirs:
+#     x: int = dir.stat().st_size
+# print(x)
