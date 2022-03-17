c146ab62486e900e47b7a18c8cd2f56a07f9b21c
Arnau Albert <arnaualbert2003@gmail.com>
2022-03-17 17:10:46 +0100

    act

--- recursion/r1.py
+++ recursion/r1.py
@@ -8,28 +8,33 @@ def say_hello_v1():
 
     while True:
         print("Hello")
+        
 #--------------------------------------------------------------------------------------------------
 def say_hello_v2():
 
     print("Hello")
     say_hello_v2()
+
 #--------------------------------------------------------------------------------------------------
 def say_hello_v3():
 
     for _ in range(5):
         print("Hello")
+
 #--------------------------------------------------------------------------------------------------
 def say_hello_v4(num_veces: int):
     num_veces: int = num_veces - 1
     print("Hello")
     if num_veces > 0:
         say_hello_v4(num_veces)
+
 #--------------------------------------------------------------------------------------------------
 def say_hello_v5(num_veces: int):
     print("Hello")
     num_veces: int
     if num_veces > 0:
         say_hello_v4(num_veces - 1)
+
 #--------------------------------------------------------------------------------------------------
 def say_hello_oficial_v1(num: int):
 
@@ -39,6 +44,7 @@ def say_hello_oficial_v1(num: int):
 
     if (num == 0):
         print("Finished!")
+
 #--------------------------------------------------------------------------------------------------
 def say_hello_oficial_v2(num: int):
 
@@ -48,6 +54,7 @@ def say_hello_oficial_v2(num: int):
 
     if (num <= 0):
         print("Finished!")    
+
 #--------------------------------------------------------------------------------------------------
 # NO TAN RECOMENDABLE
 def say_hello_oficial_v2_2(num: int):
@@ -58,6 +65,7 @@ def say_hello_oficial_v2_2(num: int):
 
     else:
         print("Finished!")    
+
 #--------------------------------------------------------------------------------------------------
 def say_hello_oficial_v2_3(num: int):
 
