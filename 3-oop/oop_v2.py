9a40836f95aefba9a92255db6280981dfe0aec81
Arnau Albert <arnaualbert2003@gmail.com>
2022-03-31 17:41:41 +0200

    act

--- 3-oop/oop_v2.py
+++ 3-oop/oop_v2.py
@@ -13,12 +13,17 @@ class BadExample():
 #methods :
 #all methods have a special parameter: self
 #when calling the method we don't write it
+#attributes:
+#attributes are variables stores inside the objects
+#they are created by the constructor 
+#to access them write self.<attribute>
 #-----------------------------------------------------
 class Dog():
 
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
+
 #----------------------------------------------------
 firulas: Dog = Dog("firulais",3)
 print(firulas.name)
