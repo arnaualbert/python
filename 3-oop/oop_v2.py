45f2ccf1a54704bfdaed056f3dd76f1909596521
Arnau Albert <arnaualbert2003@gmail.com>
2022-03-31 17:38:39 +0200

    act

--- 3-oop/oop_v2.py
+++ 3-oop/oop_v2.py
@@ -3,15 +3,23 @@ Object oriented programing
 '''
 
 # Clases empiezan en mayusculas
+
 class BadExample():
     pass
 
 # Las clases tienen un constructor
+#on definition we write __init__ ()
+#on callinf we called Dog
+#methods :
+#all methods have a special parameter: self
+#when calling the method we don't write it
+#-----------------------------------------------------
 class Dog():
 
-    #on definition we write __init__ ()
-    #on callinf we called Dog
-    #all methods have a special parameter: self
-    #when calling the method we don't write it
-    def __init__(self):
-        pass
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+#----------------------------------------------------
+firulas: Dog = Dog("firulais",3)
+print(firulas.name)
+print(firulas.age)
