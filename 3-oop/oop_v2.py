a5ecdb32f9e18d8e315c5dd32d19dcfd6ed36d7e
Arnau Albert <arnaualbert2003@gmail.com>
2022-03-31 17:32:23 +0200

    act

--- 3-oop/oop_v2.py
+++ 3-oop/oop_v2.py
@@ -8,5 +8,10 @@ class BadExample():
 
 # Las clases tienen un constructor
 class Dog():
-    def __init__(self) -> None:
+
+    #on definition we write __init__ ()
+    #on callinf we called Dog
+    #all methods have a special parameter: self
+    #when calling the method we don't write it
+    def __init__(self):
         pass
