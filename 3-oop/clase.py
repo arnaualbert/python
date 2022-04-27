62c9a8cca03c4de6e9b6e2736667777ecb67a335
Arnau Albert <arnaualbert2003@gmail.com>
2022-04-27 20:12:10 +0200

    act

--- 3-oop/clase.py
+++ 3-oop/clase.py
@@ -1,6 +1,4 @@
-
-
-
+from collections import OrderedDict
 
 # ---------------------------------------------------------------------------------------------------------------------
 class Dog:
@@ -16,9 +14,13 @@ class Dog:
 
 # Functions outside oF dog class
 # ----------------------------------------------------------------------------------------------------------------------
-def show(something):
+def show_attributes(something):
 
-    print(something.__dict__)
+    keys = sorted(something.__dict__.keys())
+    values = [something.__dict__[key] for key in keys]
+
+    for key, value in zip(keys, values):
+        print(f"{key}: {value}")
 
 
 
@@ -28,7 +30,10 @@ if __name__ == "__main__":
 
     sarah : Dog = Dog("sarah",4)
     james: Dog = Dog("James",3)
-    print(sarah, james, sep="\n")
 
-    show(sarah)
-    show(james)
+    print(sarah, james,sep="\n")
+
+    show_attributes(sarah)
+    show_attributes(james)
+    show_attributes(Dog)
+    # show(Dog)
