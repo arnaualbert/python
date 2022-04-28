18f0ec487a7bd1ec12b6905684252073e01f2779
Arnau Albert <arnaualbert2003@gmail.com>
2022-04-28 16:25:57 +0200

    act

---
+++ 3-oop/herencia_v2.py
@@ -0,0 +1,14 @@
+# classes
+class CanisLupus:
+    def __init__(self,name: str,age: int) -> None:
+        self.name = name
+        self.age = age    
+    
+    def __str__(self) -> str:
+        introduction = f"me llamo es {self.name} y tengo {self.age} años"
+
+        return f" {introduction}"
+
+# modulo
+if __name__ == "__main__":
+    pass
