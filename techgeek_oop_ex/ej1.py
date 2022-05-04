54d35322cde031e88a32a17bb911ffcb49e89bcf
Arnau Albert <arnaualbert2003@gmail.com>
2022-05-04 10:24:46 +0200

    act

---
+++ techgeek_oop_ex/ej1.py
@@ -0,0 +1,12 @@
+class Students():
+
+    def __init__(self,name: str,age: int,grade: str) -> None:
+        
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+
+
+if __name__ == "__main__":
+    raj = Students('Raj', 16, '11th')
