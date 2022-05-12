dc3c1574e79471829dbd5a207452948ef17c2b44
Arnau Albert <arnaualbert2003@gmail.com>
2022-05-12 17:18:57 +0200

    act

--- json_python/json_data.py
+++ json_python/json_data.py
@@ -13,16 +13,3 @@ print(json_str)
 print(py_str)
 
 print(py_data_2)
-
-# print(json.dumps("\"foo\bar"))
-
-# print(json.dumps('\u1234'))
-
-# print(json.dumps('\\'))
-
-# print(json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True))
-
-# from io import StringIO
-# io = StringIO()
-# json.dump(['streaming API'], io)
-# io.getvalue()
