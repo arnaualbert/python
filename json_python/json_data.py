4268a8a170bc76aeb6203dbd87d5ecbddffdddf3
Arnau Albert <arnaualbert2003@gmail.com>
2022-05-12 17:09:36 +0200

    act

---
+++ json_python/json_data.py
@@ -0,0 +1,25 @@
+import json
+import pprint
+
+data = ['foo', {'bar': ('baz', None, 1.0, 2)}]
+
+py_str = str(data)
+
+json_str = json.dumps(data)
+
+print(json_str)
+
+print(py_str)
+
+# print(json.dumps("\"foo\bar"))
+
+# print(json.dumps('\u1234'))
+
+# print(json.dumps('\\'))
+
+# print(json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True))
+
+# from io import StringIO
+# io = StringIO()
+# json.dump(['streaming API'], io)
+# io.getvalue()
