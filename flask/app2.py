681adad4cf56b0e650bc2874d57aef20753f4a6b
Arnau Albert <arnaualbert2003@gmail.com>
2022-05-18 20:03:27 +0200

    act

---
+++ flask/app2.py
@@ -0,0 +1,17 @@
+from flask import Flask, Response
+
+#1 inicializacion
+module_name = __name__
+app: Flask = Flask(__name__)
+
+#3 rutas
+@app.route("/")
+def index():
+
+    quote: str = "Tiki Taka, Jogo Bonito, Champagne, Pim Pam toma lacasitos"
+
+    return quote
+
+#2 run
+if __name__ == "__main__":
+    app.run(debug=True)
