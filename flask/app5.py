17ed662c6be898b82e039107b0eaf4a512fa4c25
Arnau Albert <arnaualbert2003@gmail.com>
2022-05-19 16:05:16 +0200

    act

---
+++ flask/app5.py
@@ -0,0 +1,26 @@
+from unicodedata import name
+from flask import Flask, Response, render_template
+
+#1 inicializacion
+module_name = __name__
+app: Flask = Flask(__name__)
+
+#3 rutas
+@app.route("/")
+def index():
+
+    saludo : str = "hola" # con render tamplate se le pasa el nombre de el archivo y las variables
+
+    return saludo
+
+@app.route("/hello/<name>")
+def saludo(name):
+
+    saludo : str = f"hola {name}"# con render tamplate se le pasa el nombre de el archivo y las variables
+
+    return saludo
+
+
+#2 run
+if __name__ == "__main__":
+    app.run(debug=True)
