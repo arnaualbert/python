1edfd230ac7b68bab5217b5038bbf9a35f0e03ea
Arnau Albert <arnaualbert2003@gmail.com>
2022-05-19 17:31:48 +0200

    act

--- flask/app8.py
+++ flask/app8.py
@@ -14,26 +14,6 @@ def index():
 
     return saludo
 
-# ruta
-@app.route("/libro/<name>/<volumen>")
-def saludo(name: str,volumen: str):
-
-    saludo : str = f"has comprado {name} volumen: {volumen}"# con render tamplate se le pasa el nombre de el archivo y las variables
-
-    return saludo
-
-#usando get
-@app.route("/libros")
-def libros():
-
-    name = request.args.get("comic")
-    volumen = request.args.get("volumen")
-
-    saludo : str = f"has comprado {name} volumen: {volumen}"# con render tamplate se le pasa el nombre de el archivo y las variables
-
-    return saludo
-
-
 
 #2 run
 if __name__ == "__main__":
