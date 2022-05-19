8253407a3e675db3076c17d05b169fcb448735dd
Arnau Albert <arnaualbert2003@gmail.com>
2022-05-19 17:54:02 +0200

    act

--- getpost/app.py
+++ getpost/app.py
@@ -6,7 +6,7 @@ from flask import Flask, Response, render_template,request
 #1 inicializacion
 module_name = __name__
 app: Flask = Flask(__name__)
-
+app.root_path # importante
 #3 rutas
 @app.route("/", methods=['GET','POST'])
 def index():
