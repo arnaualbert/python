948cdaa30574864ebaec3febcbc26301c9b77252
Arnau Albert <arnaualbert2003@gmail.com>
2022-05-19 16:12:24 +0200

    act

---
+++ flask/app6.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, Response, render_template
+
+# parametros por ruta
+
+#1 inicializacion
+module_name = __name__
+app: Flask = Flask(__name__)
+
+#3 rutas
+@app.route("/")
+def index():
+
+    saludo : str = "hola" # con render tamplate se le pasa el nombre de el archivo y las variables
+
+    return saludo
+
+@app.route("/libro/<name>/<volumen>")
+def saludo(name,volumen):
+
+    saludo : str = f"has comprado {name} volumen: {volumen}"# con render tamplate se le pasa el nombre de el archivo y las variables
+
+    return saludo
+
+
+#2 run
+if __name__ == "__main__":
+    app.run(debug=True)
